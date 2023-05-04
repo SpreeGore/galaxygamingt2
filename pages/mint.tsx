d57b5e19fec2f5109f5147d1673afabd9ce1bc9c
SpreeGore <89117156+SpreeGore@users.noreply.github.com>
2023-05-03 23:40:35 -0600

    Update v1.1

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,37 +1,8 @@
-import { Web3Button } from "@thirdweb-dev/react";
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { nftDropContractAddress } from "../consts/contractAddresses";
-import styles from "../styles/Home.module.css";
 
-const Mint: NextPage = () => {
-  const router = useRouter();
-
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.h1}>Mint a Nebula</h1>
-
-      <p className={styles.explain}>
-        Each Nebula Produces 2 GC per hour, and costs 720 GC
-      </p>
-      <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
-
-      <Web3Button
-        theme="dark"
-        contractAddress={nftDropContractAddress}
-        action={(contract) => contract.erc721.claim(1)}
-        onSuccess={() => {
-          alert("NFT Claimed!");
-          router.push("/stake");
-        }}
-        onError={(error) => {
-          alert(error);
-        }}
-      >
-        Mint a Nebula
-      </Web3Button>
-    </div>
-  );
-};
-
-export default Mint;
+<iframe
+    src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc721.html?contract=0x09AcA6FE334376DFa168adCD1dfD10aA7Fb577D5&chain=%7B%22name%22%3A%22Avalanche+C-Chain%22%2C%22chain%22%3A%22AVAX%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Favalanche.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Avalanche%22%2C%22symbol%22%3A%22AVAX%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22avax%22%2C%22chainId%22%3A43114%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22avalanche%22%7D&theme=dark&primaryColor=green"
+    width="600px"
+    height="600px"
+    style="max-width:100%;"
+    frameborder="0"
+    ></iframe>
